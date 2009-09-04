b830ba9be24d1b15b81432e6d918423c281aadcc
Petri Damstén <damu@iki.fi>
2009-09-04 18:08:07 +0300

    Don't make .git.plasmoid

--- make.sh
+++ make.sh
@@ -3,6 +3,9 @@
 rm -f *.plasmoid
 for dir in $(find . -maxdepth 1 -mindepth 1 -type d); do
     dir=${dir:2}
+    if [ "$dir" == ".git" ]; then
+        continue
+    fi
     cd $dir
     zip -9 -v -o -r ../$dir.plasmoid * -x \*~
     cd ..
