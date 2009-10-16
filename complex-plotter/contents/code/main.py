1000b0b5b8d627a9abf20697b89865a3c6541d3a
Petri Damstén <damu@iki.fi>
2009-10-16 12:00:03 +0300

    Start of resizer.

--- complex-plotter/contents/code/main.py
+++ complex-plotter/contents/code/main.py
@@ -27,11 +27,13 @@ from PyKDE4.kdecore import *
 from PyKDE4.kdeui import *
 from config import ConfigDialog
 from config import DEFAULTCFG
+from resizer import Resizer
 from helpers import *
 
-class ComplexPlotter(Applet):
+class ComplexPlotter(Applet): #, Resizer):
     def __init__(self, parent, args = None):
         Applet.__init__(self, parent)
+        #Resizer.__init__(self)
         self.cfg = {}
         self.sources = {}
         self.plotterData = {}
@@ -39,6 +41,7 @@ class ComplexPlotter(Applet):
         self.keepAlive = False
 
     def init(self):
+        #Resizer.init(self)
         # To find ui files from package dir
         UiHelper.applet = self.applet
 
