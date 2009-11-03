d674dd9ba7ebb340c209664d7e9a905700fa7777
Petri Damstén <damu@iki.fi>
2009-11-03 14:08:56 +0200

    Use name from metadata.desktop

--- link.sh
+++ link.sh
@@ -5,10 +5,11 @@ if [[ "$KDEHOME" == "" ]]; then
 fi
 
 for desktop in ./*/*.desktop; do
-    NAME=$(basename $(dirname $desktop))
+    DIR=$(basename $(dirname $desktop))
+    NAME=$(grep PluginInfo-Name $DIR/metadata.desktop | sed s/.*=//)
     cp "$desktop" "$KDEHOME/share/kde4/services/plasma-applet-$NAME.desktop"
     if [[ ! -e "$KDEHOME/share/apps/plasma/plasmoids/$NAME" ]]; then
-        ln -s "$(pwd)/$NAME/" "$KDEHOME/share/apps/plasma/plasmoids/$NAME"
+        ln -s "$(pwd)/$DIR/" "$KDEHOME/share/apps/plasma/plasmoids/$NAME"
     fi
 done
 
