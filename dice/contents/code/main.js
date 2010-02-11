9251c5484c9c6d0f2de9e89c5e908776c7acfb81
Petri Damstén <damu@iki.fi>
2010-02-11 18:08:28 +0200

    Testing some stuff...

--- dice/contents/code/main.js
+++ dice/contents/code/main.js
@@ -18,31 +18,66 @@
 SPACING = 10
 MINSIZE = 20
 
-g_layout = 0
-
 plasmoid.init = function()
 {
     print("init")
+    m_layout = 0
     plasmoid.setAspectRatioMode(KeepAspectRatio);
-    g_layout = new LinearLayout(plasmoid);
-    g_layout.setContentsMargins(0, 0, 0, 0);
-    g_layout.setItemSpacing(SPACING);
+    m_layout = new LinearLayout(plasmoid);
+    m_layout.setContentsMargins(0, 0, 0, 0);
+    m_layout.setItemSpacing(SPACING);
     plasmoid.configChanged();
+
+    // TODO No way to set my own property? Using QPropertyAnimation as QVariantAnimation.
+    m_anim = animation("Property");
+    print(m_anim);
+    m_anim.targetObject = plasmoid;
+    m_anim.valueChanged.connect(plasmoid.onValueChange);
+    m_anim.startValue = 0;
+    m_anim.endValue = 1;
+    m_anim.duration = 1000;
+
+    // TODO Applet cannot handle mouse presses? Make transparent widget for getting clicks.
+    w = new IconWidget();
+    m_layout.addItem(w);
+    w.clicked.connect(plasmoid.onClick);
+
+    m_anim.start();
+}
+
+plasmoid.onValueChange = function(value)
+{
+    print('onValueChange');
+    print(m_anim.currentValue);
+}
+
+plasmoid.onClick = function(button)
+{
+    print('onClick');
+}
+
+plasmoid.paintInterface = function(painter)
+{
+    print ('paintInterface');
+    // TODO Can't make custom widgets? Plasma::SvqWidget is too simple for this so paint it here.
+    rect = plasmoid.rect();
+    m_svg.resize(rect.width, rect.height);
+    m_svg.paint(painter, rect.x, rect.y);
 }
 
 plasmoid.configChanged = function()
 {
     print('configChanged');
-    while (g_layout.count > 0) {
-        g_layout.removeAt(0);
+    while (m_layout.count > 0) {
+        m_layout.removeAt(0);
     }
     count = plasmoid.readConfig("itemCount");
-    //g_svg = plasmoid.readConfig("itemSvg");
+    //m_svg = plasmoid.readConfig("itemSvg");
     svg = "Vegas Plasma Dice";
 
     // TODO vertical (panel)
     h = plasmoid.rect().height;
-    // Get margins
+    // TODO Only way to get margins?
     plasmoid.resize(200, 200);
     hm = 200 - plasmoid.rect().width;
     vm = 200 - plasmoid.rect().height;
@@ -50,15 +85,7 @@ plasmoid.configChanged = function()
     plasmoid.resize(hm + count * h + (count - 1) * SPACING, h + vm);
     plasmoid.setMinimumSize(hm + count * MINSIZE + (count - 1) * SPACING, MINSIZE + vm);
 
-    svg = new Svg(svg);
-    for (i = 0; i < count; ++i) {
-        item = new SvgWidget(plasmoid);
-        item.svg = svg;
-        print (item.svg);
-        //print (item.svg.imagePath);
-        //print (item.svg.elementId);
-        g_layout.addItem(item);
-    }
+    m_svg = new Svg(svg);
 }
 
 plasmoid.init();
