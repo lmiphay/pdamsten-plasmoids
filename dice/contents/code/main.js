49e59b5e59bb70d82c1625ff761e298650ef8028
Petri Damstén <damu@iki.fi>
2010-02-12 08:41:16 +0200

    Show random value

--- dice/contents/code/main.js
+++ dice/contents/code/main.js
@@ -21,7 +21,6 @@ MINSIZE = 20
 plasmoid.init = function()
 {
     print("init")
-    m_layout = 0
     plasmoid.setAspectRatioMode(KeepAspectRatio);
     m_layout = new LinearLayout(plasmoid);
     m_layout.setContentsMargins(0, 0, 0, 0);
@@ -30,50 +29,72 @@ plasmoid.init = function()
 
     // TODO No way to set my own property? Using QPropertyAnimation as QVariantAnimation.
     m_anim = animation("Property");
-    print(m_anim);
     m_anim.targetObject = plasmoid;
     m_anim.valueChanged.connect(plasmoid.onValueChange);
     m_anim.startValue = 0;
     m_anim.endValue = 1;
-    m_anim.duration = 1000;
+    m_anim.duration = 500;
+    m_anim.type = plasmoid.OutCirc;
 
     // TODO Applet cannot handle mouse presses? Make transparent widget for getting clicks.
     w = new IconWidget();
     m_layout.addItem(w);
     w.clicked.connect(plasmoid.onClick);
-
-    m_anim.start();
 }
 
 plasmoid.onValueChange = function(value)
 {
-    print('onValueChange');
-    print(m_anim.currentValue);
+    plasmoid.update();
+    if (m_anim.currentValue == 1.0) {
+        m_values = []
+        for (i = 0; i < m_count; ++i) {
+            m_values.push(Math.floor(Math.random() * 6) + 1);
+        }
+        m_anim.direction = 1;
+        m_anim.start();
+    }
 }
 
 plasmoid.onClick = function(button)
 {
     print('onClick');
+    m_anim.direction = 0;
+    m_anim.start();
+}
+
+plasmoid.paintElement = function(painter, element, opacity)
+{
+    if (opacity > 0.0) {
+        if (m_svg.hasElement(element)) {
+            rect = plasmoid.rect();
+            r = m_svg.elementRect(element);
+            if (opacity != 1.0) {
+                painter.opacity = opacity;
+            }
+            m_svg.paint(painter, r.x + rect.x, r.y + rect.y, element);
+        }
+    }
 }
 
 plasmoid.paintInterface = function(painter)
 {
-    print ('paintInterface');
     // TODO Can't make custom widgets? Plasma::SvqWidget is too simple for this so paint it here.
     rect = plasmoid.rect();
-    m_svg.resize(rect.width, rect.height);
-    m_svg.paint(painter, rect.x, rect.y);
+    if (m_svg.size != rect.size) {
+        m_svg.resize(rect.width, rect.height);
+    }
+    plasmoid.paintElement(painter, 'background', 1.0);
+    plasmoid.paintElement(painter, 'value' + m_values[0], 1.0 - m_anim.currentValue);
+    plasmoid.paintElement(painter, 'whirl', m_anim.currentValue);
 }
 
 plasmoid.configChanged = function()
 {
     print('configChanged');
-    while (m_layout.count > 0) {
-        m_layout.removeAt(0);
-    }
-    count = plasmoid.readConfig("itemCount");
-    //m_svg = plasmoid.readConfig("itemSvg");
+    m_count = plasmoid.readConfig("itemCount");
+    //svg = plasmoid.readConfig("itemSvg");
     svg = "Vegas Plasma Dice";
+    m_svg = new Svg(svg);
 
     // TODO vertical (panel)
     h = plasmoid.rect().height;
@@ -82,10 +103,13 @@ plasmoid.configChanged = function()
     hm = 200 - plasmoid.rect().width;
     vm = 200 - plasmoid.rect().height;
     // resize to item count
-    plasmoid.resize(hm + count * h + (count - 1) * SPACING, h + vm);
-    plasmoid.setMinimumSize(hm + count * MINSIZE + (count - 1) * SPACING, MINSIZE + vm);
+    plasmoid.resize(hm + m_count * h + (m_count - 1) * SPACING, h + vm);
+    plasmoid.setMinimumSize(hm + m_count * MINSIZE + (m_count - 1) * SPACING, MINSIZE + vm);
 
-    m_svg = new Svg(svg);
+    m_values = []
+    for (i = 0; i < m_count; ++i) {
+        m_values.push(1);
+    }
 }
 
 plasmoid.init();
